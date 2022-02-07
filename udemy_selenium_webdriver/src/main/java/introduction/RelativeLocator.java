5a58ab124ff4c13607dc7db133765e316db7472e
Mobolaji <bjkuppy@gmail.com>
2022-02-07 17:50:23 +0100

    using selenium relative locator to locate elements

--- udemy_selenium_webdriver/src/main/java/introduction/RelativeLocator.java
+++ udemy_selenium_webdriver/src/main/java/introduction/RelativeLocator.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
 import static org.openqa.selenium.support.locators.RelativeLocator.*;
 
 public class RelativeLocator {
@@ -15,10 +16,23 @@ public class RelativeLocator {
         WebDriver driver = new ChromeDriver();
         driver.get("https://rahulshettyacademy.com/angularpractice/");
 
-        WebElement nameEditBox = driver.findElement(By.cssSelector(".form-control.ng-pristine.ng-invalid.ng-touched"));
+        WebElement nameEditBox = driver.findElement(By.cssSelector("[name='name']"));
 
-//        String text = driver.findElement(By.tagName("label")).above(nameEditBox)).getText();
-//        System.out.println(text);
+        String text = driver.findElement(with(By.tagName("label")).above(nameEditBox)).getText();
+        System.out.println(text);
+        WebElement dateOfBirth = driver.findElement(By.cssSelector("[for='dateofBirth']"));
+        driver.findElement(with(By.tagName("input")).below(dateOfBirth)).click();
+
+        WebElement iceCreamLabel = driver.findElement(By.xpath("//label[contains(text(),'Check me out if you Love IceCreams!')]"));
+        driver.findElement(with(By.tagName("input")).toLeftOf(iceCreamLabel)).click();
+
+        WebElement radioButtonRightSide = driver.findElement(By.id("inlineRadio1"));
+        String studentText = driver.findElement(with(By.tagName("label")).toRightOf(radioButtonRightSide)).getText();
+        System.out.println(studentText);
+
+
+
+
 
 
     }
